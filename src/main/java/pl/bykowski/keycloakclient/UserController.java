166921c13dddff70a993476ee7bb07ec9861c2d2
Bykowski <przemek@bykowski.pl>
2022-06-16 01:30:33 +0200

    Initial commit

---
+++ src/main/java/pl/bykowski/keycloakclient/UserController.java
@@ -0,0 +1,30 @@
+package pl.bykowski.keycloakclient;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/for-admin")
+    @PreAuthorize("hasAuthority('admin')")
+    public String getAdmin() {
+        return "Hello Admin!";
+    }
+
+    @GetMapping("/for-user")
+    @PreAuthorize("hasAuthority('user')")
+    public String getUser() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest httpServletRequest) throws ServletException {
+        httpServletRequest.logout();
+        return "redirect: http://localhost:8081";
+    }
+}
